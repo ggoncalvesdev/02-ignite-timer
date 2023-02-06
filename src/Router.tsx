f4fc4e84c3821f1b3db99fa6102562541bab9958
Gabriel Gonçalves <gabriel.goncalves@neki-it.com.br>
2023-02-06 13:58:09 -0300

    feat(First Commit):classes e configurações default

---
+++ src/Router.tsx
@@ -0,0 +1,16 @@
+import { Routes, Route } from 'react-router-dom'
+import { DefautLayout } from './layouts/DefaultLayout'
+
+import { History } from './pages/History'
+import { Home } from './pages/Home'
+
+export function Router() {
+  return (
+    <Routes>
+      <Route path="/" element={<DefautLayout />}>
+        <Route path="/" element={<Home />} />
+        <Route path="/history" element={<History />} />
+      </Route>
+    </Routes>
+  )
+}
